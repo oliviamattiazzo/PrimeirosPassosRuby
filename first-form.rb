a438d9fe32ad35a81172f64b34e6b3d9dc72b6a4
Olivia Mattiazzo <oli.pmatt@gmail.com>
2021-07-07 11:26:07 +0100

    Atualizacao ao primeiro formulário

--- first-form.rb
+++ first-form.rb
@@ -4,15 +4,15 @@ Link: https://www.codecademy.com/catalog/language/ruby
 =end
 
 print "What's your first name? "
-first_name = gets.chomp
+first_name = gets.chomp.capitalize!
 
 print "What's your last name? "
-last_name = gets.chomp
+last_name = gets.chomp.capitalize!
 
 print "What's your city? "
-city = gets.chomp
+city = gets.chomp.capitalize!
 
 print "What's your state? "
-state = gets.chomp
+state = gets.chomp.upcase!
 
 puts "Your name is #{first_name} #{last_name} and you're from #{city}, #{state}."
