1981e946b474c570e4fe41bcec3aaba496f35866
Olivia Mattiazzo <oli.pmatt@gmail.com>
2021-07-07 11:14:46 +0100

    Primeiro form em Ruby

---
+++ first-form.rb
@@ -0,0 +1,18 @@
+=begin
+Formulário desenvolvido durante o curso de Learn Ruby, do Codecademy
+Link: https://www.codecademy.com/catalog/language/ruby
+=end
+
+print "What's your first name? "
+first_name = gets.chomp
+
+print "What's your last name? "
+last_name = gets.chomp
+
+print "What's your city? "
+city = gets.chomp
+
+print "What's your state? "
+state = gets.chomp
+
+puts "Your name is #{first_name} #{last_name} and you're from #{city}, #{state}."
